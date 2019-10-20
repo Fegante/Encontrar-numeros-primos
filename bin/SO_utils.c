35472404a0403e3dbfa954a00fe981094e6b0bda
Felipe Gante Maia de Sousa <gante.ufes@gmail.com>
2019-10-20 18:08:56 -0300

    Criando a função numero de primos

--- bin/SO_utils.c
+++ bin/SO_utils.c
@@ -5,10 +5,18 @@ void calcula_tempo(){
 
 int verifica_primo(int numero){
     int count = numero / 2;
-    int i;
-    for (i = 2; i < count; i++){
+    for (int i = 2; i < count; i++){
         if ((numero % i) == 0)
             return 0;
     }
     return 1;    
+}
+
+int numero_de_primos(int matriz[], int i, int j ){
+    int aux;
+    for ( int k = 0; k < (i+j); i++)
+    {
+        aux += verifica_primo(matriz[k]);
+    }
+    return aux;
 }
