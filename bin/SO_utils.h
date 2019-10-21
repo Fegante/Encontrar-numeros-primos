37d2bfced2a08691626d30b6a8c2395f5d62c59f
Felipe Gante Maia de Sousa <43004045+Fegante@users.noreply.github.com>
2019-10-20 23:39:12 -0300

    Update SO_utils.h

--- bin/SO_utils.h
+++ bin/SO_utils.h
@@ -1,7 +1,7 @@
 #ifndef SO_UTILS_INCLUDED
 #define SO_UTILS_INCLUDED
 
-/*Tamanho da matriz, se precisar de uma maior altere aqui.*/
+/*Tamanho da matriz, se precisar de uma maior altere aqui.1*/
 
 void calcula_tempo();
 
