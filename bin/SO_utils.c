f8ce2df56c6788116c805b7fceffc995cf038f84
Felipe Gante Maia de Sousa <gante.ufes@gmail.com>
2019-10-20 23:21:43 -0300

    Funções de criar e destruir a matriz

--- bin/SO_utils.c
+++ bin/SO_utils.c
@@ -1,6 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "SO_utils.h"
 
-void calcula_tempo(){
+int** criaMatriz(unsigned int linha, unsigned int coluna, unsigned int semente) { // cria a matriz **testada**
+	int m, n;
+	int** matriz; //cria uma matriz
+	srand(semente); // coloca um semente pré definida
+	matriz = (int**)malloc(sizeof(int*) * linha); // Um vetor de m ponteiros para inteiros
+	if (matriz == NULL) { // verifica se ainda tem espaço na memoria 
+		printf("** Error: Memoria Insuficiente **");
+		return (NULL);
+	}
+	for (m = 0; m < linha; m++) { // m vetores de n inteiros
+		matriz[m] = (int*)malloc(sizeof(int*) * coluna);
+		if (matriz[m] == NULL) { // verifica se ainda tem espaço na memoria 
+			printf("** Erro: Memoria Insuficiente **");
+			return (NULL);
+		}
+	}
+
+	for (m = 0; m < linha; m++) { // percorre as linhas da matriz
+		for (n = 0; n < coluna; n++) { // percorre as coluna da matriz 
+			matriz[m][n] = (rand() % 30000); // gera um numero entre 0  a 29999 e coloca na posição Lin x Col
+		}
+	}
+
+	return matriz;
+
+}
+
+int** destroiMatriz(unsigned int linha, unsigned int coluna, int** matriz) { // destroi a matriz **testada**
+	int i, j; // variavel axiliar;
+	if (matriz == NULL) { // verifica se a matriz não é nula
+		return (NULL);
+	}
+	if (linha < 1 || coluna < 1) { // verifica parametros recebidos 
+		printf("** Erro: Parametro invalido **\n");
+		return (matriz);
+	}
+	for (i = 0; i < linha; i++) { // libera cada linha 
+		free(matriz[i]);
+	}
+	free(matriz);
+	return (matriz);
 }
 
 int verifica_primo(int numero){
