93386c8db3d1ae5f29061253cf7e40570c8440c7
Felipe Gante Maia de Sousa <43004045+Fegante@users.noreply.github.com>
2019-10-20 23:38:06 -0300

    Update SO_utils.h

--- bin/SO_utils.h
+++ bin/SO_utils.h
@@ -1,29 +1,19 @@
+#ifndef SO_UTILS_INCLUDED
+#define SO_UTILS_INCLUDED
 
+/*Tamanho da matriz, se precisar de uma maior altere aqui.*/
 
-#define MAX_TAM 30000
-
-/*
-*
-*/
 void calcula_tempo();
 
-/*Preenche uma matriz AxB Com valores aleatorio entre 30000
-* Inputs: Matriz
-*/
-void cria_matriz( int i, int j, unsigned int semente)
-
 /*
-*
-*/
-void destroi_matriz(int linha, int coluna, float** matriz)
+Retorna 0 Caso numero não seja primo
+Retorna 1 Para o numero primo */
+int verifica_primo(int numero);
 
-/* Retorna
-*  0 Caso numero não seja primo
-*  1 Para o numero primo
-*/
-int verifica_primo(int numero)
+int numero_de_primos(int matriz[], int i, int j );
 
-/*
-*
-*/
-int numero_de_primos(int matriz[], int i, int j)
+void valores_padroes();
+
+void menu ();
+
+#endif
